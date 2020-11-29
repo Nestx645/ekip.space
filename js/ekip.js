694506416d47e23bf25b478a6c664b3353964e4f
Nestx645 <72768267+Nestx645@users.noreply.github.com>
2020-11-29 15:48:07 +0100

    Add files via upload

---
+++ js/ekip.js
@@ -0,0 +1,22 @@
+function soon() {
+    zone = document.getElementById("soon_available");
+    phrase = "Bientot disponible";
+    zone.innerText = phrase;
+}
+//Change le texte en "bientot disponible" :  onmouseover="soon()" 
+function reset() {
+    "" = document.getElementById("login");
+    "" = document.getElementById("mail");
+    "" = document.getElementById("password");
+}
+function connect() {
+    id = document.getElementById("login");
+    identifiant = id.value;
+    alert("Bonjour " + identifiant + " ! ")
+    zone = document.getElementById("form");
+    zone.innerText = identifiant;
+}
+function cacher() {
+    zone_a_cacher = document.getElementById("body");
+    zone_a_cacher.className="invisible";
+}
